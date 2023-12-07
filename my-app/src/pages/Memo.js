d2800b6d0f3b6ada48590e656b2db9809c196a69
songeunseon <ses920525@gmail.com>
2023-12-07 10:15:33 +0900

    react1207

--- my-app/src/pages/Memo.js
+++ my-app/src/pages/Memo.js
@@ -2,28 +2,32 @@ import React, { useState } from "react";
 
 const Memo = () => {
   const [txt, setTxtValue] = useState("");
+  const [Rtxt, setRtxtValue] = useState("");
   const [memoList, setMemoList] = useState([]);
-  const [checkedItems, setCheckedItems] = useState([]); //체크박스 상태
+  const [checkedItems, setCheckedItems] = useState([]); // 체크박스 상태
 
-
-  const Create = () => { //저장
-    setMemoList([...memoList, txt]);
+  const Create = () => {
+    setMemoList([...memoList, { text: txt, Rtxt: "" }]);
     setTxtValue(""); // 초기화
   };
 
-  const Update = (index) => { //수정
-    if (memoList.length > 0 && index >= 0 && index < memoList.length) {
-      const updatedList = [...memoList];
-      updatedList[index] = txt;
-      setMemoList(updatedList);
-      setTxtValue("");
-    }
+  const TxtRead = (index) => {
+    setRtxtValue(memoList[index].text); // 선택메모 내용설정
   };
 
-  const Delete = () => { //삭제
+  const Update = () => {
+    // 체크 항목 수정
+    const updatedList = memoList.map((memo, index) =>
+      checkedItems.includes(index) ? { ...memo, text: txt } : memo
+    );
+    setMemoList(updatedList);
+    setTxtValue("");
+  };
+
+  const Delete = () => {
     const updatedList = memoList.filter((_, index) => !checkedItems.includes(index));
     setMemoList(updatedList);
-    setCheckedItems([]); // 체크된 아이템 초기화
+    setCheckedItems([]); // 체크 항목 초기화
     setTxtValue("");
   };
 
@@ -38,11 +42,14 @@ const Memo = () => {
   };
 
   return (
-    <div className="card border-success mb-3" style={{ maxWidth: '18rem' }}>
+    <div className="card border-success mb-3" style={{ maxWidth: '18rem', textAlign: 'center' }}>
       <div className="card-header bg-transparent border-success">메모장</div>
       <div className="card-body text-success">
         <h5 className="card-title">오늘의 메모</h5>
-        <input type="text" value={txt} onChange={handleInputChange} />
+        <input type="text" value={txt} onChange={handleInputChange} placeholder="입력" style={{ textAlign: 'center' }} />
+        <br />
+        <br />
+        <input type="text" value={Rtxt} onChange={() => {}} placeholder="클릭한 내용 보기" style={{ textAlign: 'center' }} />
         <br />
         <br />
         <button onClick={Create}>저장</button>{" | "}
@@ -53,13 +60,15 @@ const Memo = () => {
         <ul className="list-group">
           {memoList.map((memo, index) => (
             <li key={index} className="list-group-item">
-              <input 
-              type="checkbox" 
-              id={`checkbox-${index}`}
-              checked={checkedItems.includes(index)}
-              onChange={() => handleCheckboxChange(index)}
+              <input
+                type="checkbox"
+                id={`checkbox-${index}`}
+                checked={checkedItems.includes(index)}
+                onChange={() => handleCheckboxChange(index)}
               />
-              <label htmlFor={`checkbox-${index}`}>{memo}</label>
+              <label htmlFor={`checkbox-${index}`}>
+                {memo.text} <button onClick={() => { TxtRead(index); setRtxtValue(memoList[index].text); }}>내용보기</button>
+              </label>
             </li>
           ))}
         </ul>
